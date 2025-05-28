f1f08f94fd2e2fb94e6e6160306b638f89203833
marcovsuri <86528815+marcovsuri@users.noreply.github.com>
2025-05-28 12:02:15 -0400

    Add fix router redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,14 +21,14 @@ const AnimatedRoutes = ({ isDarkMode }: { isDarkMode: boolean }) => {
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Summer isDarkMode={isDarkMode} />} />
-        {/* <Route path="/" element={<Home isDarkMode={isDarkMode} />} /> */}
+        {/* <Route path="/" element={<Home isDarkMode={isDarkMode} />} />
         <Route path="/lunch" element={<Lunch isDarkMode={isDarkMode} />} />
         <Route path="/sports" element={<Sports isDarkMode={isDarkMode} />} />
         <Route path="/summer" element={<Summer isDarkMode={isDarkMode} />} />
         <Route
           path="/exams"
           element={<ExamSchedule isDarkMode={isDarkMode} />}
-        />
+        /> */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </AnimatePresence>
